ac6149650bcc95671432a496b59ae9e73aa90268
Chai Phonbopit <phonbopit@gmail.com>
2022-04-24 15:11:25 +0700

    initial

---
+++ README.md
@@ -0,0 +1,6 @@
+Learn Terra
+---
+
+
+- [Install Local Terra](https://docs.terra.money/docs/develop/how-to/localterra/install-localterra.html)
+- [Use Terrain with LocalTerra](https://docs.terra.money/docs/develop/dapp/quick-start/using-terrain-localterra.html)
---
+++ contracts/clicker/src/msg.rs
@@ -0,0 +1,27 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InstantiateMsg {
+    pub score: i32,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    Increment {},
+    Reset { score: i32 },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    // GetCount returns the current count as a json-encoded number
+    GetScore {},
+}
+
+// We define a custom struct for each query response
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct ScoreResponse {
+    pub score: i32,
+}
